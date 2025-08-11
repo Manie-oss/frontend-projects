baa36cd2a0db61111175ca9b915d6a632f400d4a
to.mansip <to.mansip@gmail.com>
2025-08-11 07:41:00 +0000

    shows UserList component-added pagination in it

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 // import Todo from "./components/Todo";
 // import Counter from "./components/Counter";
 // import ToggleSwitch from "./components/ToggleButton";
-// import UserList from "./components/UserList";
 // import Users from "./components/Users";
 // import DropDown from './components/DropDown';
 // import Carousel from './components/Carousel';
 // import TabsComponent from './components/TabsComponent';
-import Modal from './components/Modal';
+// import Modal from './components/Modal';
+import UserList from "./components/UserList";
 
 function App(){
   
@@ -17,12 +17,12 @@ function App(){
       {/* <Todo/> */}
       {/* <Counter/> */}
       {/* <ToggleSwitch/> */}
-      {/* <UserList/> */}
       {/* <Users/> */}
       {/* <DropDown/> */}
       {/* <TabsComponent/> */}
       {/* <Carousel /> */}
-      <Modal />
+      {/* <Modal /> */}
+      <UserList/>
       </>
   )
   }
